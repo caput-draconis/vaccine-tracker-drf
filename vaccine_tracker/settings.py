a31f7977c0f8347cf3fd327d98053c51477afb0f
mrathi976 <mrathi976@apac.comcast.com>
2022-06-30 16:32:49 +0530

    Initial commit

--- vaccine_tracker/settings.py
+++ vaccine_tracker/settings.py
@@ -69,6 +69,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+        'libraries': {
+                'staticfiles': 'django.templatetags.static',
+            },
         },
     },
 ]
@@ -83,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'student_details',
-        'USER': 'studentcoordinator',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
